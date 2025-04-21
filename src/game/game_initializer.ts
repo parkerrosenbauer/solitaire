042a4225a2bd0deff07466406267ecc7f62ec8eb
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-04-21 18:22:07 -0500

    adding functionality to game initializer

--- src/game/game_initializer.ts
+++ src/game/game_initializer.ts
@@ -1,38 +1,77 @@
+import { Deck } from '../models';
 import { Pile } from '../models/pile';
 
 export type PileType = 'stock' | 'waste' | 'foundation' | 'tableau';
 
+export interface PileConfig {
+  type: PileType;
+  count: number;
+  cardsPerPile: number[];
+  flipTopCard?: boolean;
+}
 export interface GameConfig {
-  piles: {
-    type: PileType;
-    count: number;
-    cardsPerPile: number[];
-    flipTopCard?: boolean;
-  }[];
+  deck: Deck;
+  toShuffle: boolean;
+  piles: PileConfig[];
 }
 
 interface PileDto {
-  stock?: Pile[];
-  waste?: Pile[];
-  foundation?: Pile[];
-  tableau?: Pile[];
+  stock: Pile[];
+  waste: Pile[];
+  foundation: Pile[];
+  tableau: Pile[];
 }
 
 export class GameInitializer {
   private _config: GameConfig;
+  private _deck: Deck;
+  private _toShuffle: boolean;
+  private _piles: PileDto = {
+    stock: [],
+    waste: [],
+    foundation: [],
+    tableau: [],
+  };
 
   constructor(config: GameConfig) {
     this._config = config;
+    this._deck = config.deck;
+    this._toShuffle = config.toShuffle;
   }
 
   setup(): PileDto {
-    let pileConfig: PileDto = {};
+    if (this._toShuffle) this._deck.shuffle();
+    this.createPiles();
+    this.deal(
+      this.findPileConfig('tableau')!.cardsPerPile,
+      this._piles.tableau,
+    );
+    return this._piles;
+  }
+
+  private createPiles() {
     this._config.piles.forEach((pile) => {
-      pileConfig[pile.type] = [];
       for (let i = 0; i < pile.count; i++) {
-        pileConfig[pile.type]!.push(new Pile());
+        this._piles[pile.type].push(new Pile());
       }
     });
-    return pileConfig;
+  }
+
+  private findPileConfig(pileType: PileType): PileConfig | undefined {
+    this._config.piles.forEach((pile) => {
+      if (pile.type == pileType) {
+        console.log('match');
+        return pile;
+      }
+    });
+    return undefined;
+  }
+
+  private deal(cardsPerPile: number[], tableauPiles: Pile[]) {
+    for (let i = 0; i < cardsPerPile.length; i++) {
+      if (cardsPerPile[i] > i) {
+        tableauPiles[i].addCard(this._deck.draw());
+      }
+    }
   }
 }
--- src/game/spec/game_initializer.spec.ts
+++ src/game/spec/game_initializer.spec.ts
@@ -1,54 +1,74 @@
+import { Deck } from '../../models';
+import { Card, Rank, Suit } from '../../models/card';
 import { Pile } from '../../models/pile';
 import { GameConfig, PileType, GameInitializer } from '../game_initializer';
 
 describe('GameInitializer', () => {
-  const wasteType: PileType = 'waste';
-  const wasteConfig = {
-    type: wasteType,
-    count: 1,
-    cardsPerPile: [0],
+  const cards: Card[] = [];
+  const suits = Object.values(Suit) as Array<Suit>;
+  const ranks = Object.values(Rank).filter(
+    (value) => typeof value === 'number',
+  ) as Array<Rank>;
+
+  suits.forEach((suit) => {
+    ranks.forEach((rank) => {
+      cards.push(new Card(suit, rank));
+    });
+  });
+  const emptyConfigs: GameConfig = {
+    deck: new Deck([]),
+    toShuffle: false,
+    piles: [],
   };
-  const stockType: PileType = 'stock';
-  const stockConfig = {
-    type: stockType,
-    count: 1,
-    cardsPerPile: [26],
-  };
-  const foundationType: PileType = 'foundation';
-  const foundationConfig = {
-    type: foundationType,
-    count: 4,
-    cardsPerPile: [0, 0, 0, 0],
-  };
-  const tableauType: PileType = 'tableau';
-  const tableauConfig = {
-    type: tableauType,
-    count: 7,
-    cardsPerPile: [1, 2, 3, 4, 5, 6, 7],
+  const configs: GameConfig = {
+    deck: new Deck(cards),
+    toShuffle: true,
+    piles: [
+      {
+        type: 'waste',
+        count: 1,
+        cardsPerPile: [0],
+      },
+      {
+        type: 'stock',
+        count: 1,
+        cardsPerPile: [26],
+      },
+      {
+        type: 'foundation',
+        count: 4,
+        cardsPerPile: [0, 0, 0, 0],
+      },
+      {
+        type: 'tableau',
+        count: 7,
+        cardsPerPile: [1, 2, 3, 4, 5, 6, 7],
+      },
+    ],
   };
 
   it('should initialize with no piles when specified', () => {
-    const configs: GameConfig = { piles: [] };
-    const initializer = new GameInitializer(configs);
-    expect(initializer.setup()).toEqual({});
+    const initializer = new GameInitializer(emptyConfigs);
+    const { waste, stock, foundation, tableau } = initializer.setup();
+    expect(waste).toEqual([]);
+    expect(stock).toEqual([]);
+    expect(foundation).toEqual([]);
+    expect(tableau).toEqual([]);
   });
 
-  it('should initialize with a waste pile when specified', () => {
-    const configs: GameConfig = {
-      piles: [wasteConfig],
-    };
+  it('should initialize with the correct number of piles', () => {
     const initializer = new GameInitializer(configs);
-    const { waste } = initializer.setup();
-    expect(waste!.length).toBe(1);
-    expect(waste![0]).toBeInstanceOf(Pile);
+    const { waste, stock, foundation, tableau } = initializer.setup();
+    expect(waste.length).toBe(1);
+    expect(stock.length).toBe(1);
+    expect(foundation.length).toBe(4);
+    expect(tableau.length).toBe(7);
   });
 
-  it('should initialize the stock pile when specified', () => {
-    const configs: GameConfig = {
-      piles: [wasteConfig, stockConfig],
-    };
+  it('should deal the specified cards to the tableau piles', () => {
     const initializer = new GameInitializer(configs);
-    const { stock } = initializer.setup();
-    expect(stock![0]).toBeInstanceOf(Pile);
+    const { tableau } = initializer.setup();
+    expect(tableau[0].size).toBe(1);
+    expect(tableau[6].size).toBe(7);
   });
 });
