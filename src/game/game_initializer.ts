0cec8c33ab6198c5fd127df21bf397ff2ea5d5df
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-04-27 16:47:00 -0500

    refactored to separate pile logic from general setup

--- src/game/game_initializer.ts
+++ src/game/game_initializer.ts
@@ -15,6 +15,7 @@ export interface GameConfig {
   deck: Deck;
   toShuffle: boolean;
   piles: PileConfig[];
+  remainingCardPile?: PileType;
 }
 
 export class GameInitializer {
@@ -30,20 +31,16 @@ export class GameInitializer {
   constructor(config: GameConfig) {
     this._config = config;
     this._deck = config.deck;
+    this.initializePiles();
   }
 
   setup(): Record<PileType, Pile[]> {
-    if (this._config.toShuffle) this._deck.shuffle();
     try {
-      this.createPiles();
+      this.shuffleDeck();
+      this.setupPiles();
+      this.handleRemainingCards();
     } catch (error) {
-      this._deck.reset();
-      this._piles = {
-        stock: [],
-        waste: [],
-        foundation: [],
-        tableau: [],
-      };
+      this.setupRollback();
       if (error instanceof GameSetupError) {
         throw error;
       }
@@ -52,46 +49,94 @@ export class GameInitializer {
     return this._piles;
   }
 
-  private createPiles() {
-    this._config.piles.forEach((pile) => {
-      this._piles[pile.type] = [];
-      for (let i = 0; i < pile.count; i++) {
-        this._piles[pile.type].push(new Pile());
-      }
+  private initializePiles() {
+    this._piles = {
+      stock: [],
+      waste: [],
+      foundation: [],
+      tableau: [],
+    };
+  }
 
-      this.deal(pile.cardsPerPile, this._piles[pile.type]);
-      if (pile.flipTopCard) {
-        this.flipTopCard(this._piles[pile.type]);
-      }
+  private shuffleDeck() {
+    if (this._config.toShuffle) this._deck.shuffle();
+  }
+
+  private setupPiles() {
+    this._config.piles.forEach((pileConfig) => {
+      this.createPile(pileConfig);
+      this.deal(pileConfig);
+      this.flipTopCard(pileConfig);
     });
   }
 
-  private deal(cardsPerPile: number[], piles: Pile[]) {
+  private createPile(pile: PileConfig) {
+    this._piles[pile.type] = [];
+    for (let i = 0; i < pile.count; i++) {
+      this._piles[pile.type].push(new Pile());
+    }
+  }
+
+  private deal(pileConfig: PileConfig) {
+    const { toDeal, cardsPerPile, type } = pileConfig;
+    if (!toDeal) return;
+    if (!cardsPerPile) {
+      throw new GameSetupError(
+        'cardsPerPile must be specified when toDeal set to true.',
+      );
+    }
+    const piles = this._piles[type];
     let fullPiles = false;
     while (!fullPiles) {
       fullPiles = true;
       for (let i = 0; i < cardsPerPile.length; i++) {
         if (cardsPerPile[i] > piles[i].size) {
           fullPiles = false;
-          try {
-            piles[i].addCard(this._deck.draw());
-          } catch (error) {
-            if (error instanceof DeckEmptyError) {
-              throw new Error('Not enough cards to satisfy pile configs.');
-            }
-            throw error;
+          if (this._deck.isEmpty) {
+            throw new GameSetupError(
+              'Not enough cards to satisfy pile configs.',
+            );
           }
+          piles[i].addCard(this._deck.draw());
         }
       }
     }
   }
 
-  private flipTopCard(piles: Pile[]) {
+  private flipTopCard(pileConfig: PileConfig) {
+    const { type, flipTopCard } = pileConfig;
+    if (!flipTopCard) return;
+    const piles = this._piles[type];
     piles.forEach((pile) => {
       if (!pile.peek) {
-        throw new Error('Cannot flip top card of an empty pile.');
+        throw new GameSetupError('Cannot flip top card of an empty pile.');
       }
       pile.peek.flip();
     });
+  }
+
+  private handleRemainingCards() {
+    if (this._config.remainingCardPile) {
+      const remainingPile = this._piles[this._config.remainingCardPile];
+
+      if (this._deck.isEmpty) {
+        throw new GameSetupError(
+          `No cards remaining in deck to add to ${this._config.remainingCardPile} pile.`,
+        );
+      }
+      if (remainingPile.length > 1) {
+        throw new GameSetupError(
+          'More than one pile specified for remaining cards.',
+        );
+      }
+
+      this._piles[this._config.remainingCardPile][0].cards =
+        this._deck.removeAllCards();
+    }
+  }
+
+  private setupRollback() {
+    this._deck.reset();
+    this.initializePiles();
   }
 }
