074ee3bab35987d231dd8751b2183db8beb52bff
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-04-27 16:47:27 -0500

    added test cases for shuffle and remaining cards

--- src/game/spec/game_initializer.spec.ts
+++ src/game/spec/game_initializer.spec.ts
@@ -4,9 +4,9 @@ import { GameConfig, GameInitializer } from '../game_initializer';
 
 describe('GameInitializer', () => {
   let emptyConfigs: GameConfig;
-  let solitaireConfigs: GameConfig;
-  let freeCellConfigs: GameConfig;
-  const mockCardDeck: Card[] = [];
+  let configs: GameConfig;
+  let mockCardDeck: Deck;
+  const cards: Card[] = [];
   const suits = Object.values(Suit) as Array<Suit>;
   const ranks = Object.values(Rank).filter(
     (value) => typeof value === 'number',
@@ -14,63 +14,42 @@ describe('GameInitializer', () => {
 
   suits.forEach((suit) => {
     ranks.forEach((rank) => {
-      mockCardDeck.push(new Card(suit, rank));
+      cards.push(new Card(suit, rank));
     });
   });
 
   beforeEach(() => {
+    mockCardDeck = new Deck([...cards]);
     emptyConfigs = {
       deck: new Deck([]),
       toShuffle: false,
       piles: [],
     };
-    solitaireConfigs = {
-      deck: new Deck([...mockCardDeck]),
+    configs = {
+      deck: mockCardDeck,
       toShuffle: true,
       piles: [
         {
           type: 'waste',
           count: 1,
-          cardsPerPile: [0],
         },
         {
           type: 'tableau',
           count: 7,
           cardsPerPile: [1, 2, 3, 4, 5, 6, 7],
           flipTopCard: true,
+          toDeal: true,
         },
         {
           type: 'stock',
           count: 1,
-          cardsPerPile: [24],
         },
         {
           type: 'foundation',
           count: 4,
-          cardsPerPile: [0, 0, 0, 0],
         },
       ],
-    };
-    freeCellConfigs = {
-      deck: new Deck([...mockCardDeck]),
-      toShuffle: true,
-      piles: [
-        {
-          type: 'waste',
-          count: 4,
-          cardsPerPile: [0, 0, 0, 0],
-        },
-        {
-          type: 'tableau',
-          count: 8,
-          cardsPerPile: [7, 7, 7, 7, 6, 6, 6, 6],
-        },
-        {
-          type: 'foundation',
-          count: 4,
-          cardsPerPile: [0, 0, 0, 0],
-        },
-      ],
+      remainingCardPile: 'stock',
     };
   });
 
@@ -84,7 +63,7 @@ describe('GameInitializer', () => {
   });
 
   it('should initialize with the correct number of piles', () => {
-    const initializer = new GameInitializer(solitaireConfigs);
+    const initializer = new GameInitializer(configs);
     const { waste, stock, foundation, tableau } = initializer.setup();
     expect(waste.length).toBe(1);
     expect(stock.length).toBe(1);
@@ -93,28 +72,24 @@ describe('GameInitializer', () => {
   });
 
   it('should deal the specified cards to the indicated piles', () => {
-    const solitaireInitializer = new GameInitializer(solitaireConfigs);
-    const { tableau, stock } = solitaireInitializer.setup();
+    const initializer = new GameInitializer(configs);
+    const { tableau, stock } = initializer.setup();
     expect(tableau.length).toBe(7);
     expect(tableau[0].size).toBe(1);
     expect(tableau[6].size).toBe(7);
     expect(stock[0].size).toBe(24);
-
-    const freeCellInitializer = new GameInitializer(freeCellConfigs);
-    const freeCellTableau = freeCellInitializer.setup().tableau;
-    expect(freeCellTableau.length).toBe(8);
-    expect(freeCellTableau[0].size).toBe(7);
   });
 
   it('should throw an error if sum of cardsPerPile exceeds deck size', () => {
     const errorConfigs: GameConfig = {
-      deck: new Deck([...mockCardDeck]),
+      deck: mockCardDeck,
       toShuffle: true,
       piles: [
         {
           type: 'stock',
           count: 1,
           cardsPerPile: [53],
+          toDeal: true,
         },
       ],
     };
@@ -124,16 +99,58 @@ describe('GameInitializer', () => {
     );
   });
 
+  it('should throw an error if remainingCardPile is set but no cards remain', () => {
+    const errorConfigs: GameConfig = {
+      deck: mockCardDeck,
+      toShuffle: true,
+      piles: [
+        {
+          type: 'tableau',
+          count: 1,
+          cardsPerPile: [52],
+          toDeal: true,
+        },
+        {
+          type: 'stock',
+          count: 1,
+        },
+      ],
+      remainingCardPile: 'stock',
+    };
+    const initializer = new GameInitializer(errorConfigs);
+    expect(() => initializer.setup()).toThrow(
+      'Game setup failed: No cards remaining in deck to add to stock pile.',
+    );
+  });
+
+  it('should throw an error if toDeal is set to true but cardsPerPile is omitted', () => {
+    const errorConfigs: GameConfig = {
+      deck: mockCardDeck,
+      toShuffle: true,
+      piles: [
+        {
+          type: 'tableau',
+          count: 1,
+          toDeal: true,
+        },
+      ],
+    };
+    const initializer = new GameInitializer(errorConfigs);
+    expect(() => initializer.setup()).toThrow(
+      'Game setup failed: cardsPerPile must be specified when toDeal set to true.',
+    );
+  });
+
   it('should flip the top card to face up when specified', () => {
-    const initializer = new GameInitializer(solitaireConfigs);
+    const initializer = new GameInitializer(configs);
     const { tableau } = initializer.setup();
     expect(tableau[0].peek?.isFaceUp).toEqual(true);
   });
 
   it('should throw an error when attempting to flip top card of an empty pile', () => {
-    const mockDeck = new Deck([...mockCardDeck]);
+    const originalCards = [...cards];
     const errorConfigs: GameConfig = {
-      deck: mockDeck,
+      deck: mockCardDeck,
       toShuffle: true,
       piles: [
         {
@@ -148,6 +165,32 @@ describe('GameInitializer', () => {
     expect(() => initializer.setup()).toThrow(
       'Game setup failed: Cannot flip top card of an empty pile.',
     );
-    expect(mockDeck.cards).toEqual(mockCardDeck);
+    expect(mockCardDeck.cards).toEqual(originalCards);
+  });
+
+  it('should shuffle the cards when indicated', () => {
+    const mockShuffle = jest.spyOn(Deck.prototype, 'shuffle');
+    const initializer = new GameInitializer(configs);
+    initializer.setup();
+    expect(mockShuffle).toHaveBeenCalled();
+  });
+
+  it('should not shuffle the cards when indicated', () => {
+    const originalCards = [...cards];
+    const unshuffledConfigs: GameConfig = {
+      deck: mockCardDeck,
+      toShuffle: false,
+      piles: [
+        {
+          type: 'stock',
+          count: 1,
+          cardsPerPile: [52],
+        },
+      ],
+      remainingCardPile: 'stock',
+    };
+    const initializer = new GameInitializer(unshuffledConfigs);
+    const { stock } = initializer.setup();
+    expect(stock[0].cards).toEqual(originalCards);
   });
 });
