937dbc7988b204d9cefd9b06718db85cb064d0eb
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-05-04 11:25:17 -0500

    dynamic pile empty error logging

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -2,3 +2,4 @@ export { DeckEmptyError } from './deck_empty_error';
 export { PileEmptyError } from './pile_empty_error';
 export { GameSetupError } from './game_setup_error';
 export { GameError } from './game_error';
+export { GamePlayError } from './game_play_error';
