9d54c5bd88dad14ae0fbf979ac985c30c6f3f2b3
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-05-04 17:07:49 -0500

    added card not found error

--- src/errors/index.ts
+++ src/errors/index.ts
@@ -4,3 +4,4 @@ export { GameSetupError } from './game_setup_error';
 export { GameError } from './game_error';
 export { GamePlayError } from './game_play_error';
 export { GameRuleError } from './game_rule_error';
+export { CardNotFoundError } from './card_not_found_error';
