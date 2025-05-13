6e89ba9a159ba2da96fde5519f7f38c6cce79ecb
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-05-12 19:08:32 -0500

    removed need for game engine to be serialized

--- src/core/engine/game_engine.serialized.ts
+++
@@ -1,7 +0,0 @@
-import { SerializedGame } from '../game/game.serialized';
-import { GameType } from '../rules/game_type.enum';
-
-export type SerializedGameEngine = {
-  gameType: GameType;
-  game: SerializedGame;
-};
