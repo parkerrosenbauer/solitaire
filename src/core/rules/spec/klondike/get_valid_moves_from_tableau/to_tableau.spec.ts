3fdf8913b6cbda87bb451e3617245232ceedd7ec
Parker Rosenbauer <parkerrosenbauer@gmail.com>
2025-05-13 14:33:17 -0500

    reformatted spacing

--- src/core/rules/spec/klondike/get_valid_moves_from_tableau/to_tableau.spec.ts
+++ src/core/rules/spec/klondike/get_valid_moves_from_tableau/to_tableau.spec.ts
@@ -7,6 +7,7 @@ import { mockGame, pileOf } from '../../../../../utils/mocks';
 import { TABLEAU } from '../../../../../utils/pile.constants';
 import { createGameRules } from '../../../factory/game_rules.factory';
 import { GameType } from '../../../game_type.enum';
+
 describe('KlondikeRules', () => {
   const rules = createGameRules(GameType.Klondike);
   const loneTableauToTableau = createTableauToTableauRequest(0, 1, 0);
